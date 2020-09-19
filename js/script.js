289186818a764b2a9c28696df54e5eac0733e388
roverfaisal <69445568+roverfaisal@users.noreply.github.com>
2020-09-19 09:10:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    autoplay:true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
